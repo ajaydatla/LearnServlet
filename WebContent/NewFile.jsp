5e811431df2ac2f4db76aa90812a41166a5de55f
Ajay Datla <ajaydatlalbn06@gmail.com>
2020-06-03 18:28:20 +0530

    Initial commit

---
+++ WebContent/NewFile.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<form name="bmiForm" action="calculateServlet" method="POST">
+		<table>
+			<tr>
+				<td>Your Weight (kg) :</td>
+				<td><input type="text" name="weight" /></td>
+			</tr>
+			<tr>
+				<td>Your Height (m) :</td>
+				<td><input type="text" name="height" /></td>
+			</tr>
+			<th><input type="submit" value="Submit" name="find" /></th>
+			<th><input type="reset" value="Reset" name="reset" /></th>
+		</table>
+	</form>
+</body>
+</html>
---
+++ src/com/first/FormServlet.java
@@ -0,0 +1,23 @@
+package com.first;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FormServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("do post");
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("do gettt");
+		RequestDispatcher dispatcher = req.getRequestDispatcher("NewFile.jsp");
+		dispatcher.forward(req,resp);
+		//resp.sendRedirect("NewFile.jsp");
+	}
+}
